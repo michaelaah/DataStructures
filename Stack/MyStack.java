0f76ea02ec1949ce47864d5fe2e346488b5ba0fa
michaelaah <55564227+michaelaah@users.noreply.github.com>
2020-01-04 11:15:47 -0400

    MyStack file created.

---
+++ Stack/MyStack.java
@@ -0,0 +1,35 @@
+// This class implements a stack data structure.
+
+public class MyStack {
+
+    private MyLinkedList theStack;
+
+    public MyStack(){
+        this.theStack = new MyLinkedList();
+    }
+
+    public void push(Object obj){
+        this.theStack.addFirst(obj);
+    }
+
+    public Object pop(){
+       return this.theStack.removeFirst();
+    }
+
+    public Object peek(){
+        return this.theStack.getHeadData();
+    }
+
+    public boolean isEmpty(){
+        return this.theStack.isEmpty();
+    }
+
+    public String toString(){
+        String result = "";
+        if(!(this.theStack.isEmpty())){
+          result += this.theStack.toString();
+        }
+        return result;
+    }
+
+}
